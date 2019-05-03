b6648d054d0336f23a44480308c089e6f6ea133a
redVi <liberty195@gmail.com>
2019-05-03 16:52:18 +0700

    update snapshots

--- src/components/places-list-item/places-list-item.jsx
+++ src/components/places-list-item/places-list-item.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import PlaceMark from '@/components/place-mark/place-mark.jsx';
 import PlaceImage from '@/components/place-image/place-image.jsx';
 import PlaceInfo from '@/components/place-info/place-info.jsx';
 
 const PlacesListItem = ({place}) => {
+  const mark = place.premium ? <PlaceMark/> : null;
+
   return (
     <article className="cities__place-card place-card">
+      {mark}
       <PlaceImage img={place.img} />
       <PlaceInfo {...place} />
     </article>
