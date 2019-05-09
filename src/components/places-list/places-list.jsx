f3de456e93eb721dd1c5dc781be423796549ba52
redVi <liberty195@gmail.com>
2019-05-09 22:59:03 +0700

    get active list item

--- src/components/places-list/places-list.jsx
+++ src/components/places-list/places-list.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import PlacesListItem from '@/components/places-list-item/places-list-item.jsx';
 
-const PlacesList = ({offers}) => {
-  const handleImageClick = (card) => card;
+class PlacesList extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeOffer: {}
+    };
+    this._handleImageClick = this._handleImageClick.bind(this);
+  }
 
-  return (
-    <div className="cities__places-list places__list tabs__content">
-      {offers.map((offer, index) =>
-        <PlacesListItem
-          key={index}
-          offer={offer}
-          handleImageClick={handleImageClick}
-        />
-      )}
-    </div>
-  );
-};
+  _handleImageClick(activeOffer) {
+    this.setState({activeOffer});
+  }
+
+  render() {
+    const {offers} = this.props;
+
+    return (
+      <div className="cities__places-list places__list tabs__content">
+        {offers.map((offer, index) =>
+          <PlacesListItem
+            key={index}
+            offer={offer}
+            handleImageClick={this._handleImageClick}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 PlacesList.propTypes = {
   offers: PropTypes.arrayOf(PropTypes.object)
