58dce4296972209ff963655d01cf99bd50363a33
redVi <liberty195@gmail.com>
2019-05-22 22:33:37 +0700

    change props for map

--- src/components/city-map/city-map.test.js
+++ src/components/city-map/city-map.test.js
@@ -4,12 +4,9 @@ import CityMap from '@/components/city-map/city-map.jsx';
 
 describe(`CityMap`, () => {
   it(`renders correctly`, () => {
-    const offers = [
-      {name: `Canal View Prinsengracht`, price: 132, coordinates: [52.3909553943508, 4.85309666406198]},
-      {name: `Nice, cozy, warm big bed apartment`, price: 180, coordinates: [52.369553943508, 4.85309666406198]},
-    ];
-
-    const tree = renderer.create(<CityMap offers={offers} />).toJSON();
+    const tree = renderer.create(
+        <CityMap coordinates={[[52.3, 4.8], [52.3, 4.8]]} center={[52, 4]}/>
+    ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
