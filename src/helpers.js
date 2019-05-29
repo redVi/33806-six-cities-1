5cb92e9fc731f067a463580f52b0fbe01d22b77c
redVi <liberty195@gmail.com>
2019-05-29 11:34:58 +0700

    change eslint options

--- src/helpers.js
+++ src/helpers.js
@@ -1,23 +1,20 @@
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-const snakeToCamel = word => word.replace(
-  /(_\w)/g,
-  matches => matches[1].toUpperCase()
+const snakeToCamel = (word) => word.replace(
+    /(_\w)/g,
+    (matches) => matches[1].toUpperCase()
 );
 
 const normalizeKeys = (obj) => {
   if (Array.isArray(obj)) {
-    return obj.map(item => normalizeKeys(item));
+    return obj.map((item) => normalizeKeys(item));
   }
 
   if (obj !== null && obj.constructor === Object) {
-    return Object.keys(obj).reduce(
-      (result, key) => ({
-        ...result,
-        [snakeToCamel(key)]: normalizeKeys(obj[key]),
-      }),
-      {},
-    );
+    return Object.keys(obj).reduce((result, key) => ({
+      ...result,
+      [snakeToCamel(key)]: normalizeKeys(obj[key])
+    }), {});
   }
 
   return obj;
@@ -26,4 +23,4 @@ const normalizeKeys = (obj) => {
 export {
   getRandomNumber,
   normalizeKeys
-}
+};
